3c51e3321477be97133d1c286261aa0b7b57b5df
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-24 05:20:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+#Epic Bots 2022© All Rights Resived Created By Navanjana Sathsindu
+
 import os
 import random
 
@@ -7,7 +9,7 @@ from pyrogram import Client, filters
 from pyrogram.errors import *
 from pyrogram.types import *
 
-#Vars
+
 HEROKU_API_KEY = os.getenv("HEROKU_API_KEY", "")
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN")  # from @botfather
@@ -15,3 +17,5 @@ API_ID = int(os.getenv("API_ID"))  # from https://my.telegram.org/apps
 API_HASH = os.getenv("API_HASH")  # from https://my.telegram.org/apps
 AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1884885842 5115331277 5025877489 1202064253 1120271521").split())
 MONGO_URI = os.getenv("MONGO_URI")
+
+#Epic Bots 2022© All Rights Resived Created By Navanjana Sathsindu
