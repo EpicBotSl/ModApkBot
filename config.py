47449f7d322acd10a9cbec55181fa35e8f159579
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-24 05:18:12 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+import random
+
+from pyrogram.errors.exceptions.bad_request_400 import *
+from pyrogram.errors import *
+from pyrogram import Client, filters
+from pyrogram.errors import *
+from pyrogram.types import *
+
+#Vars
+HEROKU_API_KEY = os.getenv("HEROKU_API_KEY", "")
+HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # from @botfather
+API_ID = int(os.getenv("API_ID"))  # from https://my.telegram.org/apps
+API_HASH = os.getenv("API_HASH")  # from https://my.telegram.org/apps
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1884885842 5115331277 5025877489 1202064253 1120271521").split())
+MONGO_URI = os.getenv("MONGO_URI")
