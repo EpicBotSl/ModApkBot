237e9843a95d704397bd39ff1cb48eb9be9f84f5
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-24 16:16:04 +0530

    Update script.py

--- script.py
+++ script.py
@@ -6,9 +6,10 @@ from pyrogram.errors import *
 from pyrogram import Client, filters
 from pyrogram.errors import *
 from pyrogram.types import *
+from pyrogram.types import Message
 
 #Epic Bots 2022© All Rights Resived Created By Navanjana Sathsindu
-START_MG = f"Hi 🌹{message.from_user.mention}, 🌱Welcome to **Epic Mod Apk Bot⚡**"
+START_MG = f"Hi 🌹{Message.from_user.mention}, 🌱Welcome to **Epic Mod Apk Bot⚡**"
 START_BUTTON = InlineKeyboardMarkup([[              
                  InlineKeyboardButton('🆘HELP🆘', callback_data="HELP")
                  ],
