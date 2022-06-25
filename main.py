b9067c6e3584f05534d4c984a52877ec834be0b5
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-25 09:03:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -188,16 +188,19 @@ async def start(app, message):
   await message.reply_photo("https://te.legra.ph/file/d57dfa030c388a3097f49.jpg", caption=CTG_MG, reply_markup=CTG_BUTTONS)
 
 #=•=•=•=•=•=•=•=•=Categories menu regex•=•=•=•=•=•=•=•=•=•=•Epic Bots 2022© All Rights Resived•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=#
-
-@epicbot.on_message(filters.regex(pattern="SOCIAL🎭"))   
-async def startprivate(bot, message):
-     await bot.send_sticker(message.chat.id, random.choice(SOCIAL_STCR),reply_markup=SOCIAL_APS)
-
+@epicbot.on_inline_query()
 async def main():
     async with epicbot:
         # Get bot results for "hello" from the inline bot @vid
         bot_results = await epicbot.get_inline_bot_results("vid", "hello")
 
+        # Send the first result to your own chat (Saved Messages)
+        await epicbot.send_inline_bot_result(
+            "me", bot_results.query_id,
+            bot_results.results[0].id)
+
+
+app.run(main())
 
 #=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•Epic Bots 2022© All Rights Resived•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=#
 
