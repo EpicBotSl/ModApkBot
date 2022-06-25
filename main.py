691eebc70ed1e59077d3b62f9ac685b9c5eec322
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-25 13:50:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.errors import *
 from pyrogram.types import *
 from helper.decorators import humanbytes
+from advance.inline import *
 from config import *
 from script import *
 from database.db import Database
