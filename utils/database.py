854437184476c5e206755e19609ff07782528cd1
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-26 16:55:32 +0530

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -6,7 +6,7 @@ from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
 
-from config import MONGO_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER
+from config import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER
 from .helpers import unpack_new_file_id
 
 logger = logging.getLogger(__name__)
