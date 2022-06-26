38ef56ee31676f2aac2d194f73530b20676b194b
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-06-26 16:09:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,98 @@ from pyrogram.errors.exceptions.bad_request_400 import *
 from pyrogram.types import InlineQueryResultArticle
 from pyrogram.types import InputTextMessageContent
 from utils import Media, unpack_new_file_id
+
+logger = logging.getLogger(__name__)
+
+@epicbot.on_message(filters.command('start'))
+async def start(bot, message):
+    """Start command handler"""
+    if len(message.command) > 1 and message.command[1] == 'subscribe':
+        await message.reply(INVITE_MSG)
+    else:
+        buttons = [[
+            InlineKeyboardButton('Search Here', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('Go Inline', switch_inline_query=''),
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply(START_MSG, reply_markup=reply_markup)
+
+@epicbot.on_message(filters.command('channel') & filters.user(ADMINS))
+async def channel_info(bot, message):
+    """Send basic information of channel"""
+    if isinstance(CHANNELS, (int, str)):
+        channels = [CHANNELS]
+    elif isinstance(CHANNELS, list):
+        channels = CHANNELS
+    else:
+        raise ValueError("Unexpected type of CHANNELS")
+
+    text = '📑 **Indexed channels/groups**\n'
+    for channel in channels:
+        chat = await bot.get_chat(channel)
+        if chat.username:
+            text += '\n@' + chat.username
+        else:
+            text += '\n' + chat.title or chat.first_name
+
+    text += f'\n\n**Total:** {len(CHANNELS)}'
+
+    if len(text) < 4096:
+        await message.reply(text)
+    else:
+        file = 'Indexed channels.txt'
+        with open(file, 'w') as f:
+            f.write(text)
+        await message.reply_document(file)
+        os.remove(file)
+
+
+@epicbot.on_message(filters.command('total') & filters.user(ADMINS))
+async def total(bot, message):
+    """Show total files in database"""
+    msg = await message.reply("Processing...⏳", quote=True)
+    try:
+        total = await Media.count_documents()
+        await msg.edit(f'📁 Saved files: {total}')
+    except Exception as e:
+        logger.exception('Failed to check total files')
+        await msg.edit(f'Error: {e}')
+
+
+@epicbot.on_message(filters.command('logger') & filters.user(ADMINS))
+async def log_file(bot, message):
+    """Send log file"""
+    try:
+        await message.reply_document('TelegramBot.log')
+    except Exception as e:
+        await message.reply(str(e))
+
+
+@epicbot.on_message(filters.command('delete') & filters.user(ADMINS))
+async def delete(bot, message):
+    """Delete file from database"""
+    reply = message.reply_to_message
+    if not (reply and reply.media):
+        await message.reply('Reply to file with /delete which you want to delete', quote=True)
+        return
+
+    msg = await message.reply("Processing...⏳", quote=True)
+
+    for file_type in ("document", "video", "audio"):
+        media = getattr(reply, file_type, None)
+        if media is not None:
+            break
+    else:
+        await msg.edit('This is not supported file format')
+        return
+
+    file_id = unpack_new_file_id(media.file_id)[0]
+    result = await Media.collection.delete_one({'file_id': file_id})
+
+    if result.deleted_count:
+        await msg.edit('File is successfully deleted from database')
+    else:
+        await msg.edit('File not found in database')
                             
 
 
@@ -74,31 +166,7 @@ db = Database(DATABASE_URL, "epic_bot")
 #=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•Epic Bots 2022© All Risived•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=#
 #=•=•=•=Def Start=•=•=•=#
 
-@epicbot.on_message(filters.command("start"))
-async def startprivate(epicbot, message):
-    #return
-    chat_id = message.from_user.id
-    if not await db.is_user_exist(chat_id):
-        data = await epicbot.get_me()
-        BOT_USERNAME = data.username
-        await db.add_user(chat_id)
-        if -1001645328504:
-            await epicbot.send_message(
-                -1001645328504,
-                f"#NEWUSER: \n\n**User:** [{message.from_user.first_name}](tg://user?id={message.from_user.id})\n\**ID:**{message.from_user.id}\n Started @{BOT_USERNAME} !!",
-            )
-        else:
-            logging.info(f"#NewUser :- Name : {message.from_user.first_name} ID : {message.from_user.id}")
-    file_id = "CAACAgIAAxkBAAEFHSBitZsmpQpnfJ5XCFRtQ13JT74KLQACTgIAAladvQow_mttgTIDbykE"
-    await epicbot.send_sticker(message.chat.id, file_id)
-    text = f"Hi {message.from_user.mention}, 🌱Welcome to **Epic Mod Apk Bot⚡**"
-    reply_markup = START_BUTTON
-    await message.reply_text(
-        text=text,
-        reply_markup=reply_markup,
-        disable_web_page_preview=True,
-        quote=True
-    )
+
 #=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•Epic Bots 2022© All Rights Resived•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=•=#
 
 @epicbot.on_message(filters.incoming & filters.chat(-1001609244993))
